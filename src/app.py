c2d158add64a8ca9af60042bb81f39e95cd8deb2
tainaknegt <taina.knegt@gmail.com>
2025-01-14 15:57:42 -0300

    Adicionando o app.py

---
+++ src/app.py
@@ -0,0 +1,43 @@
+#Importando as bibliotecas necessárias:
+import pandas as pd
+from pathlib import Path
+import seaborn as sns
+import plotly.express as px
+import streamlit as st
+
+#Lendo os dados do df a partir de um caminho relativo
+df = pd.read_csv(Path("../Data/vehicles.csv")) 
+
+#Construindo Histograma
+fig_hist = px.histogram(df, x="odometer") 
+fig_hist.show()
+
+#Construindo gráfico de dispersão
+fig_graf = px.scatter(df, x="odometer", y="price") 
+fig_graf.show() 
+
+#Implementando botão para o Histograma 
+hist_button = st.button('Criar histograma') # criar um botão
+
+if hist_button: # se o botão for clicado escrever uma mensagem
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+            
+    # criar um histograma
+    fig_hist = px.histogram(df, x="odometer")
+        
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+#Implementando botão para o Grafico de dispersão
+disp_button = st.button('Criar Gráfico de dispersão') 
+
+if disp_button:
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+
+    # criar um gráfico
+    fig_graf = px.scatter(df, x="odometer", y="price") 
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_graf, use_container_width=True)
+
+
